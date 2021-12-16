994246a165df4e781f19a7fcd18e5c522a4434e2
gelleicsaba <gellei.csaba@gmail.com>
2021-12-16 13:13:12 +0100

    commit

--- Day-6_Lanternfish/sln-1.py
+++ Day-6_Lanternfish/sln-1.py
@@ -6,15 +6,11 @@ import os
 with open(os.path.dirname(os.path.abspath(__file__)) + "/input/my-input.txt", "r") as f:
 #with open(os.path.dirname(os.path.abspath(__file__)) + "/input/example.txt", "r") as f:
     df = f.readlines()
-
 df = df[0].strip().split(',')
 m = []
 for x in range(len(df)):
     q = (int)(df[x])
     m.append(q)
-
-print("base: ", m)
-
 ln = 80
 for x in range(ln):
     cf = 0
@@ -24,8 +20,6 @@ for x in range(ln):
         elif m[y] == 1:
             m[y] = 0
             cf = cf + 1
-
-    #print("",(x+1),"day: ",m)
     if (x == ln-1):
         break
     for q in range(len(m)):
@@ -33,6 +27,5 @@ for x in range(ln):
             m[q] = 7
     for q in range(cf):
         m.append(9)
-
 print("The result:",len(m))
 
